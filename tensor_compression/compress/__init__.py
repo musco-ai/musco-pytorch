34217d16d17cce55c15e69ee072172e53a40b624
juliagusak <y.gusak@skoltech.ru>
2019-05-10 06:44:30 +0000

    Clean up stuff

--- tensor_compression/compress/__init__.py
+++ tensor_compression/compress/__init__.py
@@ -3,6 +3,4 @@ from .cp3 import *
 from .cp4 import *
 from .compress import *
 from .rank_selection import estimate_vbmf_ranks, estimate_rank_for_compression_rate
-from .btd_tucker2 import *
-from .btd_ll1 import *
 from .svd_layer import *
